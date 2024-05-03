b886cfcda076269ca43179d7714c866cfd7e2eee
Aaron Dodson <adodson@google.com>
2024-05-03 14:05:22 -0700

    fix: avoid error when attempting to report values of nonexistent blocks

--- src/block_reporting.js
+++ src/block_reporting.js
@@ -4,10 +4,10 @@ import {Colours} from '../core/colours.js';
 export function reportValue(id, value) {
   const block = Blockly.getMainWorkspace().getBlockById(id) ||
       Blockly.getMainWorkspace().getFlyout().getWorkspace().getBlockById(id);
-  const field = block.inputList[0].fieldRow[0];
   if (!block) {
     throw 'Tried to report value on block that does not exist.';
   }
+  const field = block.inputList[0].fieldRow[0];
   const contentDiv = Blockly.DropDownDiv.getContentDiv();
   const valueReportBox = document.createElement('div');
   valueReportBox.setAttribute('class', 'valueReportBox');
