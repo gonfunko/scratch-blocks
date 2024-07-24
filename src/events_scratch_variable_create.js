83e3f37ca56017bb458a2eae5175fae47fd19553
Aaron Dodson <adodson@google.com>
2024-07-24 15:40:09 -0700

    feat: add serialization/running support to the Scratch variable creation event

--- src/events_scratch_variable_create.js
+++ src/events_scratch_variable_create.js
@@ -3,8 +3,52 @@ import * as Blockly from "blockly/core";
 class ScratchVariableCreate extends Blockly.Events.VarCreate {
   constructor(variable) {
     super(variable);
+    if (!variable) return;
+
     this.isLocal = variable.isLocal;
     this.isCloud = variable.isCloud;
+  }
+
+  toJson() {
+    const json = super.toJson();
+    json["isLocal"] = this.isLocal;
+    json["isCloud"] = this.isCloud;
+    return json;
+  }
+
+  static fromJson(json, workspace, event) {
+    const newEvent = super.fromJson(json, workspace, event);
+    newEvent.isLocal = json["isLocal"];
+    newEvent.isCloud = json["isCloud"];
+    return newEvent;
+  }
+
+  run(forward) {
+    const workspace = this.getEventWorkspace_();
+    if (forward) {
+      const VariableModel = Blockly.registry.getObject(
+        Blockly.registry.Type.VARIABLE_MODEL,
+        Blockly.registry.DEFAULT,
+        true
+      );
+      const variable = new VariableModel(
+        workspace,
+        this.varName,
+        this.varType,
+        this.varId,
+        this.isLocal,
+        this.isCloud
+      );
+      workspace.getVariableMap().addVariable(variable);
+      Blockly.Events.fire(
+        new (Blockly.Events.get(Blockly.Events.VAR_CREATE))(variable)
+      );
+    } else {
+      const variable = workspace.getVariableMap().getVariableById(this.varId);
+      if (variable) {
+        workspace.getVariableMap().deleteVariable(variable);
+      }
+    }
   }
 }
 
