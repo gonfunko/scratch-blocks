fe5ac7623c9c3d23a980b98631d97dd6f66da8d3
Aaron Dodson <adodson@google.com>
2024-07-24 15:12:22 -0700

    feat: add a custom Scratch variable model

---
+++ src/scratch_variable_model.js
@@ -0,0 +1,18 @@
+import * as Blockly from "blockly/core";
+
+class ScratchVariableModel extends Blockly.VariableModel {
+  constructor(workspace, name, type, id, isLocal, isCloud) {
+    super(workspace, name, type, id);
+    // isLocal and isCloud may not be passed when creating broadcast message
+    // variables, which conveniently are neither local nor cloud.
+    this.isLocal = !!isLocal;
+    this.isCloud = !!isCloud;
+  }
+}
+
+Blockly.registry.register(
+  Blockly.registry.Type.VARIABLE_MODEL,
+  Blockly.registry.DEFAULT,
+  ScratchVariableModel,
+  true
+);
