3add47bfec18f7f75d9a641e8b69c84e02e95822
Aaron Dodson <adodson@google.com>
2024-09-05 14:46:27 -0700

    fix: synthesize a selected theme and override CSS variables when setting a theme

--- src/renderer/constants.js
+++ src/renderer/constants.js
@@ -5,7 +5,31 @@
  */
 
 import * as Blockly from "blockly/core";
+import { cssVarify } from "../colours.js";
 
 export class ConstantProvider extends Blockly.zelos.ConstantProvider {
   REPLACEMENT_GLOW_COLOUR = "#ffffff";
+
+  setTheme(theme) {
+    const root = document.querySelector(":root");
+    for (const [key, colour] of Object.entries(theme.blockStyles)) {
+      if (typeof colour === "string") {
+        const varKey = `--colour-${key}`;
+        root.style.setProperty(varKey, colour);
+      } else {
+        theme.setBlockStyle(`${key}_selected`, {
+          colourPrimary: colour.colourQuaternary ?? colour.colourTertiary,
+          colourSecondary: colour.colourQuaternary ?? colour.colourTertiary,
+          colourTertiary: colour.colourQuaternary ?? colour.colourTertiary,
+          colourQuaternary: colour.colourQuaternary ?? colour.colourTertiary,
+        });
+      }
+    }
+    super.setTheme(theme);
+  }
+
+  createDom(svg, tagName, selector) {
+    super.createDom(svg, tagName, selector);
+    this.selectedGlowFilterId = "";
+  }
 }
