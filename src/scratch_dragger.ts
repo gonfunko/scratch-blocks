5ec7e6f72932653a00ee2b98e13a54fedcd538d4
Aaron Dodson <adodson@google.com>
2024-10-16 14:37:51 -0700

    chore: add TSDoc to ScratchDragger

--- src/scratch_dragger.ts
+++ src/scratch_dragger.ts
@@ -9,16 +9,39 @@ import { BlockDragOutside } from "./events/events_block_drag_outside.js";
 import { BlockDragEnd } from "./events/events_block_drag_end.js";
 import { isProcedureBlock, getCallers } from "./procedures";
 
+/**
+ * CSS class that allows the workspace to overflow its bounds when set.
+ */
 const BOUNDLESS_CLASS = "boundless";
 
+/**
+ * Class responsible for managing dragging items on the workspace.
+ */
 class ScratchDragger extends Blockly.dragging.Dragger {
+  /**
+   * Whether or not the current drag location is outside of the main workspace.
+   */
   draggedOutOfBounds = false;
+
+  /**
+   * Whether or not the current drag started from the flyout.
+   */
   originatedFromFlyout = false;
 
+  /**
+   * Sets the current item being dragged.
+   *
+   * @param draggable The item being dragged.
+   */
   setDraggable(draggable: Blockly.IDraggable) {
     this.draggable = draggable;
   }
 
+  /**
+   * Handles the start of a drag operation.
+   *
+   * @param event The event that triggered the drag.
+   */
   onDragStart(event: PointerEvent) {
     super.onDragStart(event);
     if (this.draggable instanceof Blockly.BlockSvg) {
@@ -39,11 +62,22 @@ class ScratchDragger extends Blockly.dragging.Dragger {
     }
   }
 
+  /**
+   * Handles motion during an ongoing drag operation.
+   *
+   * @param event The event that triggered this call.
+   * @param totalDelta The change in pointer position since the last invocation.
+   */
   onDrag(event: PointerEvent, totalDelta: Blockly.utils.Coordinate) {
     super.onDrag(event, totalDelta);
     this.updateOutOfBoundsState(event);
   }
 
+  /**
+   * Records whether or not the current drag is out of the workspace's bounds.
+   *
+   * @param event The event that triggered this call.
+   */
   updateOutOfBoundsState(event: PointerEvent) {
     if (this.draggable instanceof Blockly.BlockSvg) {
       const outOfBounds = !this.isInsideWorkspace(event);
@@ -58,6 +92,11 @@ class ScratchDragger extends Blockly.dragging.Dragger {
     }
   }
 
+  /**
+   * Handles the end of a drag.
+   *
+   * @param event The event that ended the drag.
+   */
   onDragEnd(event: PointerEvent) {
     if (
       this.draggable instanceof Blockly.BlockSvg &&
@@ -110,6 +149,14 @@ class ScratchDragger extends Blockly.dragging.Dragger {
     this.workspace.removeClass(BOUNDLESS_CLASS);
   }
 
+  /**
+   * Returns whether or not the dragged item should return to its starting
+   * position.
+   *
+   * @param event The drag event that triggered this check.
+   * @param rootDraggable The topmost item being dragged.
+   * @returns True if the draggable should return to its starting position.
+   */
   shouldReturnToStart(event: PointerEvent, rootDraggable: Blockly.IDraggable) {
     // If a block is dragged out of the workspace to be e.g. dropped on another
     // sprite, it should remain in the same place on the workspace where it was,
@@ -119,6 +166,13 @@ class ScratchDragger extends Blockly.dragging.Dragger {
     );
   }
 
+  /**
+   * Returns the root element being dragged. For shadow blocks, this is the
+   * parent block.
+   *
+   * @param draggable The element being dragged directly.
+   * @returns The element being dragged, or its parent.
+   */
   getDragRoot(draggable: Blockly.IDraggable) {
     // We can't just use getRootBlock() here because, when blocks are detached
     // from a stack via dragging, getRootBlock() still returns the root of that
@@ -130,6 +184,13 @@ class ScratchDragger extends Blockly.dragging.Dragger {
     return draggable;
   }
 
+  /**
+   * Returns whether or not the given event occurred within the bounds of the
+   * workspace.
+   *
+   * @param event The event to check.
+   * @returns True if the event occurred inside the workspace.
+   */
   isInsideWorkspace(event: PointerEvent) {
     const bounds = this.workspace.getParentSvg().getBoundingClientRect();
     const workspaceRect = new Blockly.utils.Rect(
