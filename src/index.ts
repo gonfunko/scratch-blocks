9dc8b7148b1a72127543314df338d226405f69e3
Aaron Dodson <adodson@google.com>
2024-10-17 10:56:04 -0700

    refactor: convert note.js to Typescript

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import * as Blockly from "blockly/core";
 import "./blocks/colour";
 import "./blocks/math";
 import "./blocks/matrix";
-import "./blocks/note.js";
+import "./blocks/note";
 import "./blocks/text.js";
 import "./blocks/vertical_extensions.js";
 import "./blocks/control";
